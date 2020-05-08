fceb6b7d0cfbae78bde6009e076e552a5f27b48a
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-05-08 10:48:37 -0500

    Created action + projects routers

---
+++ projects/projectRouter.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const Projects = require('../data/helpers/projectModel');
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const server = express();
 
+const projectRouter = require('./data/helpers/projectModel');
+const actionRouter = require('./data/helpers/actionModel');
+
 server.get('/', (req, res) => {
 	res.send(`<h2>Sprint Challenge!</h2>`);
 });
+
+server.use(function (res, res, next) {
+	console.log('A request just happended');
+	next();
+});
+
+server.use('/data/helpers/projects', projectRouter);
+server.use('/data/helpers/actions', actionRouter);
 
 module.exports = server;
